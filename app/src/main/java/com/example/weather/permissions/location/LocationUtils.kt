4224da5c3554a940f9f9a41d267dfaad41c149da
sujan <sujanrintu@gmail.com>
2024-05-31 22:00:16 +0530

    add locationUtils

---
+++ app/src/main/java/com/example/weather/permissions/location/LocationUtils.kt
@@ -0,0 +1,23 @@
+package com.example.weather.permissions.location
+
+import android.Manifest
+import android.content.Context
+import android.content.pm.PackageManager
+import androidx.core.content.ContextCompat
+
+class LocationUtils {
+    fun hasLocationPermission(context: Context): Boolean {
+        return (ContextCompat.checkSelfPermission(
+            context,
+            Manifest.permission.ACCESS_FINE_LOCATION
+        ) == PackageManager.PERMISSION_GRANTED
+                &&
+                ContextCompat.checkSelfPermission(
+                    context,
+                    Manifest.permission.ACCESS_COARSE_LOCATION
+                ) == PackageManager.PERMISSION_GRANTED)
+    }
+}
+
+
+
