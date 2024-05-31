ba5d8315f810a46e1ef505cdf3a6829263878c14
sujan <sujanrintu@gmail.com>
2024-05-31 23:43:02 +0530

    create SearchScreen

---
+++ app/src/main/java/com/example/weather/screen/search/SearchScreen.kt
@@ -0,0 +1,9 @@
+package com.example.weather.screen.search
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun SearchScreen(modifier: Modifier = Modifier) {
+
+}
