018dd284bb5d4166a2c186acd22119d67344df58
Sujan Mandal <sujanrintu@gmail.com>
2024-06-02 23:13:10 +0530

    api key update

--- app/src/main/java/com/example/weather/utils/Constants.kt
+++ app/src/main/java/com/example/weather/utils/Constants.kt
@@ -2,5 +2,5 @@ package com.example.weather.utils
 
 object Constants {
     const val BASE_URL = "https://api.weatherapi.com/v1/"
-    const val API_KEY = "fb65b238cf4942009ea155526241805"
+    const val API_KEY = "ecd2e21ce6864471971173634240206"
 }
